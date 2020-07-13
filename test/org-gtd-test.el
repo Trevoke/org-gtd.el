991c79a29ddfe1e4d5eca58c39ca8a6a364d58f8
Aldric Giacomoni <trevoke@gmail.com>
2020-07-12 22:38:03 -0400

    a test that the C-c c keybinding works
    *shrug* Ι needed a first test

--- test/org-gtd-test.el
+++ test/org-gtd-test.el
@@ -9,26 +9,21 @@
   (delete-directory dir t nil)
   (make-directory dir))
 
-(describe "Processing items"
-          :var ((ogt-target-dir (f-join (f-dirname (f-this-file)) "runtime-file-path")))
+(describe
+ "Processing items"
+ :var ((ogt-target-dir (f-join (f-dirname (f-this-file)) "runtime-file-path")))
 
-          (before-each
-           (setq org-gtd-directory ogt-target-dir)
-           (ogt--clean-target-directory org-gtd-directory)
-           (define-key org-gtd-command-map (kbd "C-c c") #'org-gtd-clarify-finalize)
-           (setq org-capture-templates `(("i" "GTD item"
-                                          entry (file (lambda () (org-gtd--path org-gtd-inbox-file-basename)))
-                                          "* %?\n%U\n\n  %i"
-                                          :kill-buffer t))))
+ (before-each
+  (setq org-gtd-directory ogt-target-dir)
+  (ogt--clean-target-directory org-gtd-directory)
+  (define-key org-gtd-command-map (kbd "C-c c") #'org-gtd-clarify-finalize)
+  (setq org-capture-templates `(("i" "GTD item"
+                                 entry (file (lambda () (org-gtd--path org-gtd-inbox-file-basename)))
+                                 "* %?\n%U\n\n  %i"
+                                 :kill-buffer t))))
 
-          (it "can use a keybinding to finish clarifying an item"
-              (with-simulated-input
-                  '("C-c C-c")
-                (org-gtd-capture 4 "i")
-                (insert "foobar"))
-              (org-gtd-process-inbox)
-              (with-simulated-input "t" ())
-              ;; (self-insert-command "t")
-              ;; (execute-kbd-macro (kbd "T"))
-              (execute-kbd-macro "C-c c")
-              ))
+ (it "uses a keybinding to finish clarifying an item when point is on headline"
+     (with-simulated-input '("i" "foobar" "C-c C-c" "i")
+                           (org-gtd-capture))
+     (with-simulated-input '("t" "C-c c" "RET")
+                           (org-gtd-process-inbox))))
