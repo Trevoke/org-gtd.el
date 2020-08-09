ee10b8cb70d27d01280de8f282d9602f3497b8ab
Aldric Giacomoni <trevoke@gmail.com>
2020-08-09 16:13:44 -0400

    Add GOTO and KEYS in my documentation to point to org-capture

--- org-gtd.el
+++ org-gtd.el
@@ -228,7 +228,10 @@ Done here is any done `org-todo-keyword'."
 
 (defun org-gtd-capture (&optional GOTO KEYS)
   "Capture something into the GTD inbox.
-Wraps the function `org-capture' to ensure the inbox exists."
+
+Wraps the function `org-capture' to ensure the inbox exists.
+
+For GOTO and KEYS, see `org-capture' documentation for the variables of the same name."
   (interactive)
   (kill-buffer (org-gtd--inbox-file))
   (org-capture GOTO KEYS))
