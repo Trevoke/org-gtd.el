63d6a61a7c11d08975cf065016b68efa01f8c0e7
Aldric Giacomoni <trevoke@gmail.com>
2021-12-07 13:10:16 -0500

    improve documentation for custom variables

--- org-gtd-customize.el
+++ org-gtd-customize.el
@@ -31,15 +31,19 @@
   :group 'org)
 
 (defcustom org-gtd-directory "~/gtd/"
-  "Directory of Org based GTD files.
-This is the directory where to look for the files used in
-this Org-mode based GTD implementation."
+  "Directory for org-gtd.
+
+The package will use this directory for all its functionality, whether it is
+building the agenda or refiling items."
   :group 'org-gtd
   :package-version "0.1"
   :type 'directory)
 
 (defcustom org-gtd-process-item-hooks '(org-set-tags-command)
-  "Enhancements to add to each item as they get processed from the inbox."
+  "Enhancements to add to each item as they get processed from the inbox.
+
+This is a list of functions that modify an org element. Some built-in examples
+are provided. You can create your own functions and add them to that list"
   :group 'org-gtd
   :package-version "1.0.4"
   :type 'hook
@@ -49,7 +53,11 @@ this Org-mode based GTD implementation."
   (lambda ()
    (let ((year (number-to-string (caddr (calendar-current-date)))))
      (string-join `("gtd_archive_" ,year "::datetree/"))))
-  "Function to generate archive location for org gtd."
+  "Function to generate archive location for org gtd.
+
+This function takes zero arguments. By default this generates a file
+called gtd_archive_<currentyear> in org-gtd-directory and puts the entries
+into a datetree."
   :group 'org-gtd
   :type 'sexp
   :package-version "2.0.0")
@@ -63,7 +71,11 @@ this Org-mode based GTD implementation."
       entry (file (lambda () (org-gtd-inbox-path)))
       "* %?\n%U\n\n  %i\n  %a"
       :kill-buffer t))
-  "Capture templates to be used when adding something to the inbox."
+  "Capture templates to be used when adding something to the inbox.
+
+The safe thing to modify here is the string template. Make sure there is a
+top-level heading in your template (it starts with a single asterisk) or org-gtd
+cannot be guaranteed to work well."
   :group 'org-gtd
   :type 'sexp
   :package-version "2.0.0")
@@ -77,7 +89,10 @@ this Org-mode based GTD implementation."
   :package-version "2.0.0")
 
 (defcustom org-gtd-refile-to-any-target t
-  "Set this to true if you do not need to choose where to refile processed items"
+  "Set this to true if you do not need to choose where to refile processed items.
+
+Defaults to true to carry over pre-2.0 behavior. You will need to change this
+settig if you follow the instructions to add your own refile targets."
   :group 'org-gtd
   :type 'boolean
   :package-version "2.0.0")
