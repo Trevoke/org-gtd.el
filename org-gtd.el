a4089380980740dfdef74d140804b768e08dd78f
Aldric Giacomoni <trevoke@gmail.com>
2021-12-24 16:26:07 -0600

    add a readme

--- org-gtd.el
+++ org-gtd.el
@@ -27,15 +27,15 @@
 ;; This package tries to replicate as closely as possible the GTD workflow.
 ;; This package assumes familiarity with GTD.
 ;;
-;; Upgrading? make sure you read the CHANGELOG.
-;;
 ;; This package provides a system that allows you to capture incoming things
 ;; into an inbox, then process the inbox and categorize each item based on the
 ;; GTD categories.  It leverages org-agenda to show today's items as well as the
 ;; NEXT items.  It also has a simple project management system, which currently
 ;; assumes all tasks in a project are sequential.
 ;;
-;; For a comprehensive instruction manual, see the file `README.org'.
+;; For a comprehensive instruction manual, see the documentation.
+;; Either the info file or in the doc/ directory of the repository.
+;; Upgrade information is also available therein.
 ;;
 ;;; Code:
 
