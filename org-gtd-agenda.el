29e1e5bb7e342df11b3a48ef53f7079cf672cd3e
Max Beutelspacher <max.beutelspacher@mailbox.org>
2023-02-26 22:57:39 +0100

    feat: add more org-gtd-agenda-actions

--- org-gtd-agenda.el
+++ org-gtd-agenda.el
@@ -44,74 +44,76 @@ This assumes all GTD files are also agenda files."
   (with-org-gtd-context
       (org-todo-list "NEXT")))
 
+
+(defun org-gtd-agenda--run-function (func &optional moving-operation)
+  "Run FUNC on the current agenda item.
+
+If item will be moved, set MOVING-OPERATION to t.
+Used as a helper function to run all possible actions on an item."
+  (org-agenda-check-type t 'agenda 'todo 'tags 'search)
+  (org-agenda-check-no-diary)
+  (org-agenda-maybe-loop
+   (lambda () (apply org-gtd-agenda-run-function '(func))) nil t nil
+   (let* ((marker (or (org-get-at-bol 'org-marker)
+                      (org-agenda-error)))
+          (buffer (marker-buffer marker))
+          (pos (marker-position marker)))
+     (set-marker-insertion-type marker t)
+     (org-with-remote-undo buffer
+       (with-current-buffer buffer
+         (widen)
+         (goto-char pos)
+         (when moving-operation
+           (org-up-element)
+           (org-narrow-to-element)
+           (org-fold-show-subtree)
+           (display-buffer-same-window buffer '()))
+         (apply func nil)
+         (when moving-operation (widen)
+               (winner-undo)))
+       (org-agenda-show-tags)))))
+
 ;;;###autoload
 (defun org-gtd-agenda-projectify ()
   "Transform the current agenda item into a gtd project.
 
 This function is intended to be used on incubated items that come up."
   (interactive)
-  (org-agenda-check-type t 'agenda 'todo 'tags 'search)
-  (org-agenda-check-no-diary)
-  (org-agenda-maybe-loop
-   #'org-gtd-agenda-projectify nil t nil
-   (let* ((marker (or (org-get-at-bol 'org-marker)
-                      (org-agenda-error)))
-          (buffer (marker-buffer marker))
-          (pos (marker-position marker)))
-     (set-marker-insertion-type marker t)
-     (org-with-remote-undo buffer
-       (with-current-buffer buffer
-         (widen)
-         (goto-char pos)
-         (org-up-element)
-         (org-narrow-to-element)
-         (org-show-subtree)
-         (display-buffer-same-window buffer '())
-         (org-gtd--project)
-         (widen)
-         (winner-undo))
-       (org-agenda-show-tags)))))
+  (org-gtd-agenda--run-function 'org-gtd--project t))
 
 ;;;###autoload
 (defun org-gtd-agenda-delegate ()
   "Delegate current agenda task."
   (interactive)
-  (org-agenda-check-type t 'agenda 'todo 'tags 'search)
-  (org-agenda-check-no-diary)
-  (org-agenda-maybe-loop
-   #'org-gtd-agenda-delegate nil t nil
-   (let* ((marker (or (org-get-at-bol 'org-marker)
-                      (org-agenda-error)))
-          (buffer (marker-buffer marker))
-          (pos (marker-position marker)))
-     (set-marker-insertion-type marker t)
-     (org-with-remote-undo buffer
-       (with-current-buffer buffer
-         (widen)
-         (goto-char pos)
-         (org-gtd-delegate))
-       (org-agenda-show-tags)))))
+  (org-gtd-agenda--run-function 'org-gtd-delegate))
 
 ;;;###autoload
 (defun org-gtd-agenda-cancel-project ()
   "Cancel the project that has the highlighted task."
   (interactive)
-  (org-agenda-check-type t 'agenda 'todo 'tags 'search)
-  (org-agenda-check-no-diary)
-  (org-agenda-maybe-loop
-   #'org-gtd-agenda-cancel-project nil t nil
-   (let* ((marker (or (org-get-at-bol 'org-marker)
-                      (org-agenda-error)))
-          (buffer (marker-buffer marker))
-          (pos (marker-position marker)))
-     (set-marker-insertion-type marker t)
-     (org-with-remote-undo buffer
-       (with-current-buffer buffer
-         (widen)
-         (goto-char pos)
-         (org-up-heading-safe)
-         (org-gtd-cancel-project))
-       (org-agenda-show-tags)))))
+  (org-gtd-agenda--run-function (lambda () (progn (org-up-heading-safe) (org-gtd-cancel-project)))))
+
+;;;###autoload
+(defun org-gtd-agenda-trash ()
+  "Move the current agenda item to trash."
+  (interactive)
+  (org-gtd-agenda--run-function 'org-gtd--trash t))
+
+;;;###autoload
+(defun org-gtd-agenda-single-action ()
+  "Transform the current agenda item into a single action.
+
+This function is intended to be used on incubated items that come up."
+  (interactive)
+  (org-gtd-agenda--run-function 'org-gtd--single-action t))
+
+;;;###autoload
+(defun org-gtd-agenda-quick-action ()
+  "Process the current agenda item by doing it now.
+
+This function is intended to be used on incubated items that come up."
+  (interactive)
+  (org-gtd-agenda--run-function 'org-gtd--quick-action t))
 
 (provide 'org-gtd-agenda)
 ;;; org-gtd-agenda.el ends here
