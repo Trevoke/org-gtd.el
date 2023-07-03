b9945284b4e3943a960606ef87dddfb5c6177401
yetanotherfossman <83962798+yetanotherfossman@users.noreply.github.com>
2023-07-03 15:47:05 -0300

    Fix process with empty inbox
    Signed-off-by: yetanotherfossman <83962798+yetanotherfossman@users.noreply.github.com>

--- org-gtd-process.el
+++ org-gtd-process.el
@@ -46,8 +46,9 @@
           (goto-char (point-min))
           (when (org-before-first-heading-p)
             (outline-next-visible-heading 1))
-          (org-N-empty-lines-before-current 1)
-          (org-gtd-clarify-inbox-item))
+          (when (org-at-heading-p)
+            (org-N-empty-lines-before-current 1)
+            (org-gtd-clarify-inbox-item)))
       (user-error (org-gtd-process--stop)))))
 
 ;;;; Functions
