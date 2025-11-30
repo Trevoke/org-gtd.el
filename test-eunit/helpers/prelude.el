c3afaea6de38cacf4d017256b168f071b116f41c
Aldric Giacomoni <trevoke@gmail.com>
2025-11-29 20:43:22 -0500

    move tests for task shared between projects

--- test-eunit/helpers/prelude.el
+++ test-eunit/helpers/prelude.el
@@ -27,18 +27,36 @@
 (require 'mock-fs)
 (require 'org-gtd)
 
-;; E-unit setup with mock-fs integration
-(require 'ogt-eunit-setup (file-name-concat default-directory "test-eunit/helpers/setup.el"))
+;; Helper function to get path relative to this prelude file
+;; This file is at test-eunit/helpers/prelude.el
+(eval-and-compile
+  (defun ogt-eunit--relative-path (relative-path)
+    "Get absolute path RELATIVE-PATH from this file's directory."
+    (let* ((file-loc (or load-file-name
+                         byte-compile-current-file
+                         buffer-file-name))
+           (base-dir (when file-loc (file-name-directory file-loc))))
+      (if base-dir
+          (expand-file-name relative-path base-dir)
+        (error "Cannot determine prelude.el location")))))
+
+;; E-unit setup with mock-fs integration (same directory)
+(require 'ogt-eunit-setup (ogt-eunit--relative-path "setup.el"))
 
 ;; Reuse framework-agnostic helpers from buttercup test suite
-(require 'org-gtd-test-helper-clarifying (file-name-concat default-directory "test/helpers/clarifying.el"))
-(require 'org-gtd-test-helper-processing (file-name-concat default-directory "test/helpers/processing.el"))
-(require 'org-gtd-test-helper-builders (file-name-concat default-directory "test/helpers/builders.el"))
-(require 'org-gtd-test-helper-wip (file-name-concat default-directory "test/helpers/wip.el"))
-(require 'org-gtd-test-helper-utils (file-name-concat default-directory "test/helpers/utils.el"))
+;; Navigate from test-eunit/helpers/ to test/helpers/ via ../../test/helpers/
+;; NOTE: Load order matters! builders.el must be loaded BEFORE processing.el
+;; because processing.el has a require for builders with default-directory,
+;; which fails during Eldev compilation. By loading builders first, the
+;; require in processing.el becomes a no-op (already provided).
+(require 'org-gtd-test-helper-builders (ogt-eunit--relative-path "../../test/helpers/builders.el"))
+(require 'org-gtd-test-helper-clarifying (ogt-eunit--relative-path "../../test/helpers/clarifying.el"))
+(require 'org-gtd-test-helper-processing (ogt-eunit--relative-path "../../test/helpers/processing.el"))
+(require 'org-gtd-test-helper-wip (ogt-eunit--relative-path "../../test/helpers/wip.el"))
+(require 'org-gtd-test-helper-utils (ogt-eunit--relative-path "../../test/helpers/utils.el"))
 
 ;; Domain assertions (query functions are framework-agnostic)
-(require 'ogt-assertions (file-name-concat default-directory "test/helpers/assertions.el"))
+(require 'ogt-assertions (ogt-eunit--relative-path "../../test/helpers/assertions.el"))
 
 (provide 'ogt-eunit-prelude)
 
