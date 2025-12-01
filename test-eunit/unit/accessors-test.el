70f1ae6e0d4dc55eb1e231fa8259ad46854fe734
Aldric Giacomoni <trevoke@gmail.com>
2025-11-30 19:46:32 -0500

    more tests

--- test-eunit/unit/accessors-test.el
+++ test-eunit/unit/accessors-test.el
@@ -173,6 +173,233 @@
     (goto-char (point-min))
     (assert-equal "Projects" (org-gtd-get-task-category "project-1"))))
 
+;;;; Property Writers - org-gtd-set-task-state
+
+(deftest accessors/set-state-todo-to-next ()
+  "Changes task state from TODO to NEXT."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (org-gtd-set-task-state "task-1" "NEXT")
+    (assert-equal "NEXT" (org-gtd-get-task-state "task-1"))))
+
+(deftest accessors/set-state-next-to-done ()
+  "Changes task state from NEXT to DONE."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* NEXT Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (org-gtd-set-task-state "task-1" "DONE")
+    (assert-equal "DONE" (org-gtd-get-task-state "task-1"))))
+
+(deftest accessors/set-state-returns-nil-for-nonexistent ()
+  "Returns nil for non-existent task."
+  (accessors-eunit--setup)
+  (assert-nil (org-gtd-set-task-state "non-existent" "DONE")))
+
+;;;; Property Writers - org-gtd-add-to-multivalued-property
+
+(deftest accessors/add-multivalued-first-value ()
+  "Adds first value to empty property."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (org-gtd-add-to-multivalued-property "task-1" "ORG_GTD_DEPENDS_ON" "blocker-1")
+    (assert-equal '("blocker-1") (org-gtd-get-task-dependencies "task-1"))))
+
+(deftest accessors/add-multivalued-to-existing ()
+  "Adds value to existing property."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":ORG_GTD_DEPENDS_ON: blocker-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (org-gtd-add-to-multivalued-property "task-1" "ORG_GTD_DEPENDS_ON" "blocker-2")
+    (assert-equal '("blocker-1" "blocker-2") (org-gtd-get-task-dependencies "task-1"))))
+
+(deftest accessors/add-multivalued-throws-for-nonexistent ()
+  "Throws error for non-existent task."
+  (accessors-eunit--setup)
+  (assert-raises 'user-error
+   (org-gtd-add-to-multivalued-property "non-existent" "ORG_GTD_DEPENDS_ON" "blocker-1")))
+
+;;;; Property Writers - org-gtd-remove-from-multivalued-property
+
+(deftest accessors/remove-multivalued-value ()
+  "Removes value from property."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":ORG_GTD_DEPENDS_ON: blocker-1 blocker-2\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (org-gtd-remove-from-multivalued-property "task-1" "ORG_GTD_DEPENDS_ON" "blocker-1")
+    (assert-equal '("blocker-2") (org-gtd-get-task-dependencies "task-1"))))
+
+(deftest accessors/remove-multivalued-throws-for-nonexistent ()
+  "Throws error for non-existent task."
+  (accessors-eunit--setup)
+  (assert-raises 'user-error
+   (org-gtd-remove-from-multivalued-property "non-existent" "ORG_GTD_DEPENDS_ON" "blocker-1")))
+
+;;;; Task Lookup - org-gtd-find-task-in-current-buffer
+
+(deftest accessors/find-task-in-buffer ()
+  "Finds task in current buffer."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task 1\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (insert "* TODO Task 2\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-2\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (let ((marker (org-gtd-find-task-in-current-buffer "task-2")))
+      (assert-true marker)
+      (assert-equal (current-buffer) (marker-buffer marker)))))
+
+(deftest accessors/find-task-returns-nil-when-not-found ()
+  "Returns nil for task not in current buffer."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-nil (org-gtd-find-task-in-current-buffer "non-existent"))))
+
+;;;; Task State Predicates - org-gtd-task-is-done-p
+
+(deftest accessors/is-done-true-for-done ()
+  "Returns truthy for DONE task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* DONE Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-true (org-gtd-task-is-done-p "task-1"))))
+
+(deftest accessors/is-done-true-for-cncl ()
+  "Returns truthy for CNCL task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* CNCL Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-true (org-gtd-task-is-done-p "task-1"))))
+
+(deftest accessors/is-done-nil-for-todo ()
+  "Returns nil for TODO task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-nil (org-gtd-task-is-done-p "task-1"))))
+
+(deftest accessors/is-done-nil-for-next ()
+  "Returns nil for NEXT task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* NEXT Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-nil (org-gtd-task-is-done-p "task-1"))))
+
+(deftest accessors/is-done-nil-for-nonexistent ()
+  "Returns nil for non-existent task."
+  (accessors-eunit--setup)
+  (assert-nil (org-gtd-task-is-done-p "non-existent")))
+
+;;;; Task State Predicates - org-gtd-task-is-active-p
+
+(deftest accessors/is-active-true-for-todo ()
+  "Returns truthy for TODO task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-true (org-gtd-task-is-active-p "task-1"))))
+
+(deftest accessors/is-active-true-for-next ()
+  "Returns truthy for NEXT task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* NEXT Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-true (org-gtd-task-is-active-p "task-1"))))
+
+(deftest accessors/is-active-nil-for-done ()
+  "Returns nil for DONE task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* DONE Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-nil (org-gtd-task-is-active-p "task-1"))))
+
+(deftest accessors/is-active-nil-for-wait ()
+  "Returns nil for WAIT task."
+  (accessors-eunit--setup)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* WAIT Task\n")
+    (insert ":PROPERTIES:\n")
+    (insert ":ID: task-1\n")
+    (insert ":END:\n")
+    (goto-char (point-min))
+    (assert-nil (org-gtd-task-is-active-p "task-1"))))
+
 (provide 'accessors-test)
 
 ;;; accessors-test.el ends here
--- test/accessors-test.el
+++ test/accessors-test.el
@@ -7,12 +7,9 @@
 
 ;;; Commentary:
 ;;
-;; Unit tests for org-gtd accessor layer that wraps org-mode operations.
+;; All tests in this file have been migrated to test-eunit/unit/accessors-test.el
 ;;
-;; These tests focus on the accessor layer's ability to read and write
-;; properties, find tasks, and provide clean interfaces to org-mode state.
-;;
-;; Test Coverage:
+;; Test Coverage (now in e-unit):
 ;; - Property readers (get dependencies, blockers, projects, state, category)
 ;; - Property writers (set state, add/remove multivalued properties)
 ;; - Task lookup (find task markers)
@@ -21,236 +18,25 @@
 
 ;;; Code:
 
-(require 'buttercup)
-(require 'org-gtd-accessors)
-(require 'org-gtd-value-objects)
-(require 'org-gtd-core)
-
-;;;; Test Setup
-
-(defun accessors-test--setup ()
-  "Set up minimal test environment for accessor tests."
-  (setq org-todo-keywords '((sequence "TODO" "NEXT" "WAIT" "|" "DONE" "CNCL"))
-        org-done-keywords '("DONE")
-        org-gtd-keyword-mapping '((todo . "TODO")
-                                  (next . "NEXT")
-                                  (wait . "WAIT")
-                                  (canceled . "CNCL"))))
-
-;;;; Property Readers - MIGRATED to test-eunit/unit/accessors-test.el
-;; Migrated tests (12 total):
+;; All tests migrated to test-eunit/unit/accessors-test.el (31 tests total):
+;;
+;; Property Readers (12 tests):
 ;; - org-gtd-get-task-dependencies (4 tests)
 ;; - org-gtd-get-task-blockers (2 tests)
 ;; - org-gtd-get-task-state (4 tests)
 ;; - org-gtd-get-task-category (2 tests)
-
-;;;; Property Writers
-
-(describe "org-gtd-set-task-state"
-
-  (before-each (accessors-test--setup))
-
-  (it "changes task state from TODO to NEXT"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (org-gtd-set-task-state "task-1" "NEXT")
-      (expect (org-gtd-get-task-state "task-1") :to-equal "NEXT")))
-
-  (it "changes task state from NEXT to DONE"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* NEXT Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (org-gtd-set-task-state "task-1" "DONE")
-      (expect (org-gtd-get-task-state "task-1") :to-equal "DONE")))
-
-  (it "returns nil for non-existent task"
-    (expect (org-gtd-set-task-state "non-existent" "DONE") :to-be nil)))
-
-(describe "org-gtd-add-to-multivalued-property"
-
-  (before-each (accessors-test--setup))
-
-  (it "adds first value to empty property"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (org-gtd-add-to-multivalued-property "task-1" "ORG_GTD_DEPENDS_ON" "blocker-1")
-      (expect (org-gtd-get-task-dependencies "task-1") :to-equal '("blocker-1"))))
-
-  (it "adds value to existing property"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":ORG_GTD_DEPENDS_ON: blocker-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (org-gtd-add-to-multivalued-property "task-1" "ORG_GTD_DEPENDS_ON" "blocker-2")
-      (expect (org-gtd-get-task-dependencies "task-1")
-              :to-equal '("blocker-1" "blocker-2"))))
-
-  (it "throws error for non-existent task"
-    (expect (org-gtd-add-to-multivalued-property "non-existent" "ORG_GTD_DEPENDS_ON" "blocker-1")
-            :to-throw 'user-error)))
-
-(describe "org-gtd-remove-from-multivalued-property"
-
-  (before-each (accessors-test--setup))
-
-  (it "removes value from property"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":ORG_GTD_DEPENDS_ON: blocker-1 blocker-2\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (org-gtd-remove-from-multivalued-property "task-1" "ORG_GTD_DEPENDS_ON" "blocker-1")
-      (expect (org-gtd-get-task-dependencies "task-1") :to-equal '("blocker-2"))))
-
-  (it "throws error for non-existent task"
-    (expect (org-gtd-remove-from-multivalued-property "non-existent" "ORG_GTD_DEPENDS_ON" "blocker-1")
-            :to-throw 'user-error)))
-
-;;;; Task Lookup
-
-(describe "org-gtd-find-task-in-current-buffer"
-
-  (before-each (accessors-test--setup))
-
-  (it "finds task in current buffer"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task 1\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (insert "* TODO Task 2\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-2\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (let ((marker (org-gtd-find-task-in-current-buffer "task-2")))
-        (expect marker :not :to-be nil)
-        (expect (marker-buffer marker) :to-be (current-buffer)))))
-
-  (it "returns nil for task not in current buffer"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-find-task-in-current-buffer "non-existent") :to-be nil))))
-
-;;;; Task State Predicates
-
-(describe "org-gtd-task-is-done-p"
-
-  (before-each (accessors-test--setup))
-
-  (it "returns truthy for DONE task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* DONE Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-done-p "task-1") :to-be-truthy)))
-
-  (it "returns truthy for CNCL task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* CNCL Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-done-p "task-1") :to-be-truthy)))
-
-  (it "returns nil for TODO task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-done-p "task-1") :to-be nil)))
-
-  (it "returns nil for NEXT task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* NEXT Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-done-p "task-1") :to-be nil)))
-
-  (it "returns nil for non-existent task"
-    (expect (org-gtd-task-is-done-p "non-existent") :to-be nil)))
-
-(describe "org-gtd-task-is-active-p"
-
-  (before-each (accessors-test--setup))
-
-  (it "returns truthy for TODO task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* TODO Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-active-p "task-1") :to-be-truthy)))
-
-  (it "returns truthy for NEXT task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* NEXT Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-active-p "task-1") :to-be-truthy)))
-
-  (it "returns nil for DONE task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* DONE Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-active-p "task-1") :to-be nil)))
-
-  (it "returns nil for WAIT task"
-    (with-temp-buffer
-      (org-mode)
-      (insert "* WAIT Task\n")
-      (insert ":PROPERTIES:\n")
-      (insert ":ID: task-1\n")
-      (insert ":END:\n")
-      (goto-char (point-min))
-      (expect (org-gtd-task-is-active-p "task-1") :to-be nil))))
+;;
+;; Property Writers (8 tests):
+;; - org-gtd-set-task-state (3 tests)
+;; - org-gtd-add-to-multivalued-property (3 tests)
+;; - org-gtd-remove-from-multivalued-property (2 tests)
+;;
+;; Task Lookup (2 tests):
+;; - org-gtd-find-task-in-current-buffer (2 tests)
+;;
+;; Task State Predicates (9 tests):
+;; - org-gtd-task-is-done-p (5 tests)
+;; - org-gtd-task-is-active-p (4 tests)
 
 (provide 'accessors-test)
 
